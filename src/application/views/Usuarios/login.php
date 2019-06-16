f6407a1cd21be87b7e4d7d51499fefa6940b3cd2
Raphael Ramos <raphael.opensource@gmail.com>
2019-06-15 21:03:58 -0300

    Tela de login

--- src/application/views/Usuarios/login.php
+++ src/application/views/Usuarios/login.php
@@ -3,23 +3,32 @@
 <head>
 	<meta charset="UTF-8">
 	<title>APH - Assistente de Planejamento de Horários</title>
-	<link rel="stylesheet" href="<?=base_url('assets/css/loginpage.css')?>">
 	<link rel="stylesheet" href="<?=base_url('assets/css/bootstrap.min.css')?>">
+	<link rel="stylesheet" href="<?=base_url('assets/css/loginpage.css')?>">
+	<link rel="stylesheet" href="<?=base_url('assets/css/alertas.css')?>">
 </head>
 <body>
-	<?php if(!empty($this->session->flashdata('invalido'))):?>
-		<div><?=$this->session->flashdata('invalido');?></div>
-	<?php endif;?>
-
-	<form method="POST" action="<?=base_url('Usuarios/autenticar')?>">
-		<fieldset>
-			<input type="text" id="matricula" name="matricula" placeholder="Matrícula" required><br>
-			<input type="password" name="senha" placeholder="Senha" required>
-		
-			<input type="submit" value=	"ENTRAR">
-		</fieldset>
-		
-	</form>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 offset-md-4" style="text-align:center;">
+				<img src="<?=base_url('assets/img/LogoAPH.png')?>" height="200vh" alt="">
+				<form method="POST" action="<?=base_url('Usuarios/autenticar')?>">
+					<?php if(!empty($this->session->flashdata('invalido'))):?>
+						<div class="invalido"><?=$this->session->flashdata('invalido');?></div>
+					<?php endif;?>
+					<div class="form-group">
+						<input class="form-control campo" type="text" id="matricula" name="matricula" placeholder="Matrícula" required>
+					</div>
+					<div class="form-group">
+						<input class="form-control campo" type="password" name="senha" placeholder="Senha" required>
+					</div>
+					<div class="form-group">
+						<input class="form-control botao" type="submit" value="ENTRAR">
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
 
 	<script src="<?=base_url('assets/js/jquery.js')?>"></script>
 	<script src="<?=base_url('assets/js/bootstrap.min.js')?>"></script>

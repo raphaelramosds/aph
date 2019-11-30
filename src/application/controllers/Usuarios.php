b1395cf3c91b92fe8515c05288646657d8d03da0
raphaelramosds <raphael.opensource@gmail.com>
2019-11-30 12:16:04 -0300

    Colocando os dois tipos de autenticação

--- src/application/controllers/Usuarios.php
+++ src/application/controllers/Usuarios.php
@@ -1,78 +1,4 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class Usuarios extends CI_Controller 
-{
-
-	public function __construct()
-	{
-		parent::__construct();
-		$this->load->model('UsuariosModel','usuarios');
-		$this->load->model('GruposModel','grupos');
-
-	}
-
-	public function login()
-	{
-		$this->load->view('Usuarios/login');
-	}
-
-	public function autenticar()
-	{
-
-	    $matricula = $this->input->post('matricula');
-	    $senha = $this->input->post('senha');
-
-		// 	Autenticação do administrador
-		if($this->input->post('responsavel') == 1)
-		{
-			$condicoes = array(
-				'matricula' => $matricula,
-				'senha' => $senha,
-				'membro_comis' => 2
-			);
-			$this->db->where($condicoes);
-	
-			$retorno = $this->db->get('acha_pro')->row_array();
-			if(empty($retorno))
-			{
-				$this->session->set_flashdata('invalido','Dados inválidos. Credenciais não pertencem a nenhum administrador');
-				redirect('Usuarios/login');
-			}
-			else
-			{
-				$this->session->set_userdata('usuario',$retorno);
-				redirect('admin');
-			}
-		}
-		// Autenticação do docente
-		else
-		{
-			$this->db->where('matricula', 	$matricula);
-			$this->db->where('senha', $senha);
-			$this->db->where('membro_comis !=', 2);
-
-			$encontrar_usuario = $this->db->get('acha_pro')->row_array();
-
-			if($encontrar_usuario != NULL)
-			{
-				$this->session->set_userdata('usuario',$encontrar_usuario);
-				redirect('home');
-			}
-
-			else
-			{
-				$this->session->set_flashdata('invalido','Credenciais inválidas. Tente novamente');
-				redirect('usuarios/login');
-			}
-	
-		}
-
-	}
-
-}
-
-
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
@@ -93,7 +19,9 @@ class Usuarios extends CI_Controller
 	{
 	    $matricula = $this->input->post('matricula');
 	    $senha = $this->input->post('senha');
+
 		// 	Autenticação do administrador
+
 		if($this->input->post('responsavel') == 1)
 		{
 			$condicoes = array(
@@ -115,7 +43,33 @@ class Usuarios extends CI_Controller
 				redirect('admin');
 			}
 		}
-		// Autenticação do docente
+
+		// Autenticação normal
+		else
+		{
+			$this->db->where('matricula', 	$matricula);
+			$this->db->where('senha', $senha);
+			$this->db->where('membro_comis !=', 2);
+
+			$encontrar_usuario = $this->db->get('acha_pro')->row_array();
+
+			if($encontrar_usuario != NULL)
+			{
+				$this->session->set_userdata('usuario',$encontrar_usuario);
+				redirect('home');
+			}
+
+			else
+			{
+				$this->session->set_flashdata('invalido','Credenciais inválidas. Tente novamente');
+				redirect('usuarios/login');
+			}
+		}
+
+		// Autenticação via SUAP
+
+		/*
+
 		else
 		{
 			$requisicao = array(
@@ -214,5 +168,5 @@ class Usuarios extends CI_Controller
 	    $status = json_decode($response, true);
 	    return $status;
 	}
-
+	*/
 }
